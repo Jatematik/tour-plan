a571b9753996fe0856c68d55e1ef379323fa3f89
Jatematik <freerun_dimon@mail.ru>
2020-08-04 13:40:08 +0600

    create: add validation form

--- js/main.js
+++ js/main.js
@@ -46,21 +46,42 @@ $(document).ready(function () {
     var targetModal = $(this).attr("data-href");
     $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
     $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+    $(document).keyup(function (event) {
+      if (event.which == "27") {
+        closeModal(event);
+      }
+    });
   }
 
   function closeModal(event) {
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-
-    $(document).keyup(function (event) {
-      if (event.which == "27") {
-        $(modalOverlay).removeClass("modal__overlay--visible");
-        $(modalDialog).removeClass("modal__dialog--visible");
-      }
-    });
-
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  // Обработка форм
+
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Укажите имя",
+          minlength: "имя должно быть не короче 2 букв",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Телефон обязателен",
+        },
+      },
+    });
+  });
+
+  $(document).ready(function () {
+    $(".phone").mask("+7(999)999-99-99");
+  });
 });
